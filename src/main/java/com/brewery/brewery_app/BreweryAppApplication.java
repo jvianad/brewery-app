b0dbdd9278c5da384f4485b0086604dfe122af1f
Jean Viana <jeanvianademares@gmail.com>
2025-02-24 15:57:20 -0500

    Commit1: Se crea proyecto inicial

---
+++ src/main/java/com/brewery/brewery_app/BreweryAppApplication.java
@@ -0,0 +1,13 @@
+package com.brewery.brewery_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brewery/brewery_app/BreweryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brewery.brewery_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
